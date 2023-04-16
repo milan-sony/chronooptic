3c3f1ab00818721507789faafbf37adfbf230649
Milan Sony <milanfrom2001@gmail.com>
2023-04-16 20:10:09 +0530

    new html page created, new function added to check the hour added

--- chronooptic.py
+++ chronooptic.py
@@ -3,11 +3,15 @@ from notifypy import Notify
 import pyttsx3
 import webbrowser
 import pyautogui
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 # work time is in minutes
 work_time = 1
 # rest time in in seconds
 rest_time = 20
+# flag = 0
 
 # function to convert minute to second
 def convert_minute_to_second():
@@ -25,24 +29,33 @@ def notification(message):
   notification.send()
 
 # open web browser function
-def web():
+def webpage1():
   try:
-    webbrowser.open_new_tab("index.html")
+    webbrowser.open_new_tab("page1.html")
     flag = 1
     if flag == 1:
       sleep(rest_time)
       pyautogui.hotkey('ctrl', 'w')
       sleep(0.5)
       pyautogui.hotkey('alt', 'tab')
-      # minute_checker()
       hour_checker()
   except webbrowser.Error:
     message = "Someting went wrong with the browser"
     notification(message)
 
-def hour():
-  print("Its been 1 hr")
-  minute_checker()
+def webpage2():
+  try:
+    webbrowser.open_new_tab("page2.html")
+    flag = 1
+    if flag == 1:
+      sleep(rest_time)
+      pyautogui.hotkey('ctrl', 'w')
+      sleep(0.5)
+      pyautogui.hotkey('alt', 'tab')
+      minute_checker()
+  except webbrowser.Error:
+    message = "Someting went wrong with the browser"
+    notification(message)
 
 time_count = 1
 
@@ -54,7 +67,7 @@ def hour_checker():
     minute_checker()
   else:
     time_count = 1
-    hour()
+    webpage2()
 
 # text to speech function
 def tts():
@@ -67,18 +80,11 @@ def tts():
   engine.setProperty('voice', voices[0].id)
   engine.say(f"Hey, you have been using this system for {work_time} minute. Relax and take some rest")
   engine.runAndWait()
-  web()
+  webpage1()
 
 # function to track work_time
 def minute_checker():
   worktime = convert_minute_to_second()
-  # while worktime:
-  #   # timer
-  #   mins, secs = divmod(worktime, 60)
-  #   timer = '{:02d}:{:02d}'.format(mins, secs)
-  #   print(timer, end="\r")
-  #   sleep(1)
-  #   worktime -= 1
   sleep(worktime)
   message = f"You have been using this system for {work_time} minute"
   notification(message)
